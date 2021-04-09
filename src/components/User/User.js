c84b600099963c043b71a7aebc7a1ffc2096a09e
bishnunathbd <bishnunath774@gmail.com>
2021-04-09 14:12:43 +0600

    38-9-explore more about conditional rendering

---
+++ src/components/User/User.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const User = () => {
+  return (
+    <div>
+      <div>
+        <h2>Greetings</h2>
+        <p>Welcome, my friend.</p>
+        <p>Who the hell are you?</p>
+      </div>
+
+      <div>
+        <h2>Food</h2>
+        <p>I will buy food for you.</p>
+        <p>Buy food for me.</p>
+      </div>
+
+      <div>
+        <h2>Connection</h2>
+        <p>Let's join my facebook.</p>
+      </div>
+    </div>
+  );
+};
+
+export default User;
